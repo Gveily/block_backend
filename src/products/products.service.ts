104293810179f339cc7c75a7a3915f7f74165dc1
Mikita Kalashnikau <nikita.kalashnikau@aetsoft.net>
2023-05-02 02:34:24 +0200

    Update API

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -20,6 +20,41 @@ export class ProductsService {
     )
   }
 
+  getProductsWithCounter(products) {
+    const mapper = {};
+    const multidimensionalGroupedArray = [[]];
+
+    products.forEach(el => {
+      if (!(el.baseProductId + el.weight in mapper)) {
+        mapper[el.baseProductId + el.weight] = 0;
+      }
+
+      if (el.baseProductId + el.weight in mapper) {
+        mapper[el.baseProductId + el.weight] += 1;
+      }
+    });
+
+    for(let prop in mapper) {
+      const el = products.find((product) => product.baseProductId + product.weight === prop);
+
+      for (let i = 0; i < parseInt(mapper[prop]); i++) {
+        multidimensionalGroupedArray[multidimensionalGroupedArray.length - 1].push(el);
+      }
+
+      multidimensionalGroupedArray.push([]);
+    }
+
+    multidimensionalGroupedArray.pop();
+
+    return multidimensionalGroupedArray.map(el => {
+      const res = {
+        ...el[0],
+        amount: el.length
+      }
+      return res;
+    })
+  }
+
   async findByAreaId(areaId: number) {
     const productsByAreaId = await this.productsRepository.find({
       where: {
@@ -31,10 +66,11 @@ export class ProductsService {
         id: null,
         weight: null,
         price: null,
+        baseProductId: null
       },
     });
 
-    return productsByAreaId;
+    return this.getProductsWithCounter(productsByAreaId);
   }
 
   async findOne(id: number) {
